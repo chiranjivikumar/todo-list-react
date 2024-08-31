1fdbe3eff68fe2bac34d986799f0fe8a1d067614
Chiranjivi999 <145798258+Chiranjivi999@users.noreply.github.com>
2024-08-31 16:04:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+ 
+import Todos from './Components/Todos';
+import Todo from './Components/Todo';
+ 
+
+
+function App() {
+  return (
+   <>
+    {/* <Todos/> */}
+    <Todo/>
+  
+   </>
+  );
+}
+
+export default App;
